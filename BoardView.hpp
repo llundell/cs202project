cbed605704b545dab2ccd288c66773ed7dd8cba2
llundell <llundell@ymail.com>
2017-11-21 09:41:01 -0900

    Create BoardView.hpp

---
+++ BoardView.hpp
@@ -0,0 +1,31 @@
+//
+//  BoardView.hpp
+//  CS202Connect4
+//
+//  Created by Laura on 11/20/17.
+//  Copyright © 2017 Laura. All rights reserved.
+//
+
+#ifndef BoardView_hpp
+#define BoardView_hpp
+
+#include <stdio.h>
+#include <SFML/Graphics.hpp>
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 1024
+
+class BoardView {
+    
+public:
+    BoardView();
+    ~BoardView();
+
+    void draw();
+    
+private:
+
+    
+};
+
+
+#endif /* BoardView_hpp */
